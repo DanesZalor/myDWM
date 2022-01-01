57578f695da568cc9b3178fd6a5884d91213cfe6
Daniel James Dolor <estheryahe@gmail.com>
2022-01-01 19:13:28 +0800

    finish almost everything i think

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#place this in .local/share/dwm/
 xrandr --otput eDP-1 --mode 1920x1080 --gamma 1.0:1.0:1.0 &
 feh --bg-fill ~/.local/share/dwm/dwmbinbo.png &
 /.aur/dwm/scripts/statusLoop &
